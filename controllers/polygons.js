d31e7b9f67842ca4bee7d117ce1b7b291c89b031
DusanSacha <sacha.dusan@gmail.com>
2016-08-19 14:03:43 +0200

    zoom parameter in /map change

--- controllers/polygons.js
+++ controllers/polygons.js
@@ -3,6 +3,7 @@ var Polygon = require('../models/Polygon.js');
 exports.getPolygons = function(req,res) {
 	var type = req.body.type;
 	var coordinates = req.body.coordinates;
+	var zoom = req.body.zoom;
 	var screen = {
 	    geometry: {
 	       	$geoIntersects: {
@@ -11,7 +12,8 @@ exports.getPolygons = function(req,res) {
 	             	coordinates: coordinates,
 	        	}
 	        }
-	    }
+	    },
+	    zoom:zoom
 	};
 
 	Polygon.find(screen,function (err, trends) {
