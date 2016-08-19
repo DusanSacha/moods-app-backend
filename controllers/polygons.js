4a1c2e7912613a261fc3f337967c1c05e8be3b1f
DusanSacha <sacha.dusan@gmail.com>
2016-08-19 09:50:42 +0200

    zoom parameter in /map

--- controllers/polygons.js
+++ controllers/polygons.js
@@ -3,12 +3,14 @@ var Polygon = require('../models/Polygon.js');
 exports.getPolygons = function(req,res) {
 	var type = req.body.type;
 	var coordinates = req.body.coordinates;
+	var zoom = req.body.zoom;
 	var screen = {
 	    geometry: {
 	       	$geoIntersects: {
 	          	$geometry: {
-	            	type: type ,
-	             	coordinates: coordinates
+	            	type: type,
+	             	coordinates: coordinates,
+	             	zoom:zoom
 	        	}
 	       }
 	    }
