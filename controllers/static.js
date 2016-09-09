3949eb887c97742b0e6f39e5ebe02708fdd2042a
dominik.bigl@cadaxo.com <dominik.bigl@cadaxo.com>
2016-09-09 16:18:00 +0200

    resulttype

--- controllers/static.js
+++ controllers/static.js
@@ -10,13 +10,14 @@ exports.staticData = function(req,res) {
 	  	res.sendStatus(500);
 	  	console.error(err);
 	  } else {
-    
       trends[0].trendlistCount = [];
       trends[0].trendlist.forEach(function(sHashtag) {
         trends[0].trendlistCount.push([sHashtag,"123"]);    
 			});
       console.log("trends");
+      console.log(trends[0]);
       console.log(typeof trends);
+      console.log(typeof trends[0]);
       //array of Objects
 	  	res.send(trends);
 	  }
