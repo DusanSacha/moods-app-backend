6c2f57401fcff5e952b7141e1465bd7822324ff9
DusanSacha <sacha.dusan@gmail.com>
2016-11-22 13:47:16 +0100

    dev static

--- controllers/static.js
+++ controllers/static.js
@@ -31,3 +31,42 @@ exports.staticData = function(req,res) {
 	  }
 	});
 };
+
+exports.staticDataDev = function(req,res) {
+  var oTrends = null;
+  var aTrendsRes = [];
+	StaticTrend.find(function (err, trends) {
+	  if (err) {
+	  	res.sendStatus(500);
+	  	console.error(err);
+	  } else {
+	  	  
+	  	  trends.forEach(function(oTrend) {
+	  	  	var oNewTrend = oTrend.toObject(); 
+			console.log(oNewTrend);
+	  	  	oNewTrend.trendlistCount = [[oNewTrend.trendlist[0],"101","101 Moods"],[oNewTrend.trendlist[1],"72","72 Moods"],[oNewTrend.trendlist[2],"62","62 Moods"]];
+	  	  	aTrendsRes.push(oNewTrend);
+	  	  	
+	  	  });
+	  	  res.send(aTrendsRes);
+
+	   //    oTrends = trends[0].toObject();
+	   //    oTrends.trendlistCount = [];
+	      
+	   //    var aPromises = [];
+
+	   //    oTrends.trendlist.forEach(function(sHashtag) {
+	   //      	aPromises.push(Mood.count({"hashtag": sHashtag}).exec());
+		  // });
+
+		  // Promise.all(aPromises).then(function (result){
+		  //     oTrends.trendlist.forEach(function(sHashtag,iIndex) {
+		  //       oTrends.trendlistCount.push([sHashtag,result[iIndex],MainController.getCountText(result[iIndex])]);
+				//     });
+		  //     res.send(oTrends);
+		  // }).catch(function(err) {
+	  	// 		   console.log(err.message);
+		  // });
+	  }
+	});
+};
